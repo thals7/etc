b216d56d8c54f9878127652ed5f12de3d100d0af
thals7 <thals_7@naver.com>
2020-09-23 02:28:48 +0900

    pygame_project

--- 나도코딩/pygame_basic/Quiz_2.py
+++ 나도코딩/pygame_basic/Quiz_2.py
@@ -27,15 +27,32 @@ character_x = (screen_width / 2) - (character_width / 2)
 character_y = screen_height - character_height
 
 character_speed = 0.6
-# enemy 불러오기
-enemy = pygame.image.load("C:/Users/dfjun/workspace/somin_study/etc/나도코딩/pygame_basic/enemy.png")
-enemy_size = enemy.get_rect().size
-enemy_width = enemy_size[0]
-enemy_height = enemy_size[1]
-enemy_x = random.randint(0,(screen_width-enemy_width))
-enemy_y = 0
 
-enemy_speed = 0.4
+# multiple enemy 클래스 정의
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self) # 스프라이트 생성 위해 무조건 필요함
+        self.image = pygame.image.load("C:/Users/dfjun/workspace/somin_study/etc/나도코딩/pygame_basic/enemy.png")
+        self.rect = self.image.get_rect() # 이미지와 동일한 직사각형 생성
+        self.size = self.rect.size # 적 사이즈
+        self.width = self.size[0] # 적 가로
+        self.height = self.size[1] # 적 세로
+        self.x = random.randint(0, (screen_width - self.width)) # 적 생성시 x 위치
+        self.y = 0 # 적 생성시 y 위치 (화면 가장 위)
+        self.speed = random.uniform(0.1, 1.0) # 적 생성시 적의 스피드 (랜덤)
+    def update(self):
+        self.y = self.speed * clock.tick(30) # (loop 한번 돌 때 적의 스피드)
+        if self.y >= screen_height: #적이 화면에서 사라졌을때 다시 적 하나를 생성함
+            self.x = random.randint(0, (screen_width - self.width))
+            self.y = 0
+            self.speed = random.uniform(0.1, 1.0)
+
+e1 = Enemy()
+e2 = Enemy()
+e3 = Enemy()
+
+enemies = [e1, e2, e3]
+enemies_group = pygame.sprite.RenderPlain(*enemies)
 
 # 이동할 좌표
 to_x = 0
@@ -60,7 +77,6 @@ while running:
                 to_x = 0
 
     character_x += to_x * dt
-    enemy_y += enemy_speed * dt
 
     # 가로 경계값 처리
     if character_x < 0:
@@ -68,27 +84,29 @@ while running:
     elif character_x > screen_width - character_width:
         character_x = screen_width - character_width
 
-    if enemy_y >= screen_height:
-        enemy_x = random.randint(0,(screen_width-enemy_width))
-        enemy_y = 0
+    # 적 재생성
+    if enemies_group.y >= screen_height:
+        enemies_group.x = random.randint(0, (screen_width - enemies_group.width))
+        enemies_group.y = 0
 
     # 4. 충돌 처리
     character_rect = character.get_rect()
     character_rect.left = character_x
     character_rect.top = character_y
 
-    enemy_rect = enemy.get_rect()
-    enemy_rect.left = enemy_x
-    enemy_rect.top = enemy_y
+    enemies_group.rect = enemies_group.image.get_rect()
+    enemies_group.rect.left = enemies_group.x
+    enemies_group.rect.top = enemies_group.y
 
-    if character_rect.colliderect(enemy_rect):
+
+    if character_rect.colliderect(enemies_group.rect):
         print("Game Over")
         running = False
 
     # 5. 화면에 그리기
     screen.blit(background, (0,0))
     screen.blit(character, (character_x, character_y))
-    screen.blit(enemy, (enemy_x, enemy_y))
+    screen.blit(e1, (enemies_group, enemies_group._x, enemies_group.y))
     pygame.display.update()
 
 pygame.quit()
